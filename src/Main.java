ccdea58b728ce75253681c57d5e8ef6fa241ed74
Aidarchik <ishimgrad@gmail.com>
2025-07-04 10:55:50 +0200

    добавление функции вычисления с обрабткой исключений

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input:");
+        String input = scanner.nextLine();
+        try {
+            String result = calc(input);
+            System.out.println("Output:");
+            System.out.println(result);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            scanner.close();
         }
     }
 
+    public static String calc(String input) throws Exception {
+        input = input.trim();
 
-    public static String calc(String input){
-        return "";
+        // Проверим, содержит ли выражение ровно два операнда и один оператор
+        String[] tokens = input.split(" ");
+        if (tokens.length != 3) {
+            throw new Exception("throws Exception // Неверный формат. Должно быть два операнда и один оператор");
+        }
+
+        int a, b;
+        String operator = tokens[1];
+
+        try {
+            a = Integer.parseInt(tokens[0]);
+            b = Integer.parseInt(tokens[2]);
+        } catch (NumberFormatException e) {
+            throw new Exception("throws Exception // Операнды должны быть целыми числами");
+        }
+
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            throw new Exception("throws Exception // Числа должны быть от 1 до 10 включительно");
+        }
+
+        int result;
+
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b; // целочисленное деление
+                break;
+            default:
+                throw new Exception("throws Exception // Недопустимая операция");
+        }
+
+        return String.valueOf(result);
     }
 }
